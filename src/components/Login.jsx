16212aff2b8fd6bfd6fe03f3a71f6d80b9f2a5e6
KevinC911 <00157320@uca.edu.sv>
2023-06-14 12:18:54 -0600

    adding the QRGen and QRScan, all responsive

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -26,21 +26,21 @@ function Login({userName, onChangeUser}) {
 
     return(
         <div className="w-screen h-screen grid bg-gray-200 font-rubik">
-            <form onSubmit={submitLogin} className="place-self-center flex flex-col items-center rounded-sm border-2 w-2/4 h-screen">
+            <form onSubmit={submitLogin} className="place-self-center flex flex-col items-center rounded-sm border-2 w-10/12 md:w-2/4 h-screen">
                 <Link to='/'>
                     <img src="https://i.ibb.co/4M3W5Vp/Logo.png" className="w-40 h-16 hover:cursor-pointer mt-4"/>
                 </Link>
                 <strong className="text-xl mt-8">Iniciar sesión</strong>
                 <input name="identifier" 
                 type="text" 
-                className="w-1/2 h-8 mt-2 border-2 border-gray-400 outline-blue-500 rounded-sm pl-4 text-gray-600" 
+                className="w-full md:w-1/2 h-8 mt-2 border-2 border-gray-400 outline-blue-500 rounded-sm pl-4 text-gray-600" 
                 placeholder="Nombre de usuario o email"
                 value={username}
                 onChange={(e) => setUsername(e.target.value)}
                 ></input>
                 <input name="password" 
                 type="text" 
-                className="w-1/2 h-8 mt-2 border-2 border-gray-400 outline-blue-500 rounded-sm pl-4 text-gray-600" 
+                className="w-full md:w-1/2 h-8 mt-2 border-2 border-gray-400 outline-blue-500 rounded-sm pl-4 text-gray-600" 
                 placeholder="Contraseña"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}></input>
@@ -53,7 +53,7 @@ function Login({userName, onChangeUser}) {
                 </button>
                 <hr className="mt-8 w-3/4 flex flex-row border border-gray-300"/>
                 <h2 className="mt-8">¿Nuevo en Billet?</h2>
-                <button className="w-2/4 h-12 flex flex-row items-center bg-white hover:border-blue-500 hover:text-blue-500 border-2 border-gray-400">
+                <button className="w-full md:w-1/2 h-12 flex flex-row items-center bg-white hover:border-blue-500 hover:text-blue-500 border-2 border-gray-400">
                     <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/2008px-Google_%22G%22_Logo.svg.png"
                     className="h-8 ml-8 justify-self-center"/>
                     <strong className="mr-6 justify-self-center grow">Registrarse con Google</strong>
